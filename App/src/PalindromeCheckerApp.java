c436b900458103daaa7ccda0d5e533866ab2b878
rohit-6833 <ra6833@srmist.edu.in>
2026-03-11 02:34:01 +0530

    UC1:Application entry and Welcome

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeApp {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Version 1");
+        System.out.println("System intialiastion");
+    }
+}
